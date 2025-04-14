b8db3833250e704fdecb5929f0f679d7590dff61
Namkyung Koo <united94@naver.com>
2025-04-14 20:11:25 +0900

    feat: setup exchange-service project

---
+++ exchange-service/src/test/java/com/namkyung/exchange_service/ExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namkyung.exchange_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
