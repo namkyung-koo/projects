7ae9577c35d69cc4876d1f05435832b20f69d052
Namkyung Koo <united94@naver.com>
2025-04-18 19:53:09 +0900

    feat: add findByUserId method

--- exchange-service/src/main/java/com/namkyung/exchange_service/repository/UserRepository.java
+++ exchange-service/src/main/java/com/namkyung/exchange_service/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package com.namkyung.exchange_service.repository;
 
 import com.namkyung.exchange_service.domain.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {}
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUserId(String userId);
+}
