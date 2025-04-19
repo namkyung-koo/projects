12b10635cd0281b0376ddb10e8b600bd17cddbba
Namkyung Koo <united94@naver.com>
2025-04-19 19:52:48 +0900

    feat: add showExchangeRates method

--- exchange-service/src/main/java/com/namkyung/exchange_service/controller/CurrencyController.java
+++ exchange-service/src/main/java/com/namkyung/exchange_service/controller/CurrencyController.java
@@ -1,24 +1,37 @@
 package com.namkyung.exchange_service.controller;
 
 import com.namkyung.exchange_service.service.CurrencyService;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ExchangeRateController {
+public class CurrencyController {
 
     private final CurrencyService currencyService;
 
-    public ExchangeRateController(CurrencyService currencyService) {
+    public CurrencyController(CurrencyService currencyService) {
         this.currencyService = currencyService;
     }
 
     @GetMapping("/exchange-rate")
     public String showExchangeRates(Model model) {
         Map<String, String> exchangeRates = currencyService.getExchangeRatesInKRWFormatted();
+
+        ZonedDateTime nowUtc = ZonedDateTime.now(ZoneOffset.UTC);
+        ZonedDateTime nowKst = nowUtc.withZoneSameInstant(ZoneId.of("Asia/Seoul"));
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
         model.addAttribute("exchangeRates", exchangeRates);
+        model.addAttribute("utcTime", nowUtc.format(formatter));
+        model.addAttribute("kstTime", nowKst.format(formatter));
+
         return "exchange-rate";
     }
 }
