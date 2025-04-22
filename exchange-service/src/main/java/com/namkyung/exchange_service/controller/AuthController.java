bb1c267a6b3a9a0c37e3574a530d28c9b57b0f46
Namkyung Koo <united94@naver.com>
2025-04-22 16:33:48 +0900

    feat: modify AuthController

--- exchange-service/src/main/java/com/namkyung/exchange_service/controller/AuthController.java
+++ exchange-service/src/main/java/com/namkyung/exchange_service/controller/AuthController.java
@@ -1,19 +1,27 @@
 package com.namkyung.exchange_service.controller;
 
-import com.namkyung.exchange_service.domain.User;
+import com.namkyung.exchange_service.domain.entity.User;
+import com.namkyung.exchange_service.exception.AbsentUserIdException;
+import com.namkyung.exchange_service.exception.DuplicateUserIdException;
+import com.namkyung.exchange_service.exception.WrongPasswordException;
+import com.namkyung.exchange_service.service.AuthService;
 import com.namkyung.exchange_service.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class AuthController {
 
     private final UserService userService;
+    private final AuthService authService;
 
     @GetMapping("/")
     public String index() {
@@ -27,15 +35,21 @@ public class AuthController {
     }
 
     // 로그인 처리 (POST /login)
-//    @PostMapping("/login")
-//    public String login(@RequestParam String userId, @RequestParam String password, Model model) {
-//        User user = userService.findByUserIdAndPassword(userId, password);
-//        if (user == null) {
-//            return "redirect:/login?error";
-//        }
-//        model.addAttribute("username", user.getUsername());
-//        return "main";
-//    }
+    @PostMapping("/login")
+    public String login(@RequestParam String userId,
+                        @RequestParam String password,
+                        HttpSession session,
+                        Model model) {
+        try {
+            User user = authService.login(userId, password);
+            session.setAttribute("loginUser", user);
+            model.addAttribute("username", user.getUsername());
+            return "main";
+        } catch (AbsentUserIdException | WrongPasswordException e) {
+            log.warn("로그인 실패: {}", e.getMessage());
+            return "redirect:/login?error";
+        }
+    }
 
     // 회원 가입 페이지 이동 (GET /signup)
     @GetMapping("/signup")
@@ -50,15 +64,21 @@ public class AuthController {
                                @RequestParam String password,
                                @RequestParam String confirmPassword) {
         if (password.equals(confirmPassword)) {
-            userService.register(username, userId, password);
-            return "redirect:/login";
+            try {
+                userService.register(username, userId, password);
+                return "redirect:/login";
+            } catch (DuplicateUserIdException e) {
+                log.warn("로그인 실패: {}", e.getMessage());
+                return "redirect:/signup?error";
+            }
         }
         return "redirect:/signup?error";
     }
 
     // 로그아웃
     @GetMapping("/logout")
-    public String logout() {
-        return "/redirect:/index";
+    public String logout(HttpSession session) {
+        session.invalidate(); // 로그인 정보 제거
+        return "redirect:/";
     }
 }
