614903dcaee3ee6789ec2f7435ae24c05a1e313d
Namkyung Koo <united94@naver.com>
2025-04-22 16:32:46 +0900

    feat: modify Repositories

--- exchange-service/src/main/java/com/namkyung/exchange_service/repository/ExchangeTransactionRepository.java
+++ exchange-service/src/main/java/com/namkyung/exchange_service/repository/ExchangeTransactionRepository.java
@@ -1,7 +1,6 @@
 package com.namkyung.exchange_service.repository;
 
-import com.namkyung.exchange_service.domain.ExchangeTransaction;
-import lombok.Getter;
+import com.namkyung.exchange_service.domain.entity.ExchangeTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExchangeTransactionRepository extends JpaRepository<ExchangeTransaction, Integer> {}
--- exchange-service/src/main/java/com/namkyung/exchange_service/repository/UserRepository.java
+++ exchange-service/src/main/java/com/namkyung/exchange_service/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.namkyung.exchange_service.repository;
 
-import com.namkyung.exchange_service.domain.User;
+import com.namkyung.exchange_service.domain.entity.User;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
